2336d939604beda0804fb55960b2207d8da49e31
Nityananda Gohain <nityanandagohain@gmail.com>
2019-01-16 17:17:20 +0530

    chat integration with firebase complete

--- lib/pages/chatPage.dart
+++ lib/pages/chatPage.dart
@@ -8,12 +8,58 @@ class ChatPage extends StatefulWidget {
   _ChatPageState createState() => _ChatPageState();
 }
 
-class _ChatPageState extends State<ChatPage> {
-  final TextEditingController textEditingController =
+class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
+  final List<Msg> _messages = <Msg>[];
+  final TextEditingController _textEditingController =
       new TextEditingController();
-  @override
+
+  //Subscribe so that any change in the collection will automatically reflect here
+  StreamSubscription<QuerySnapshot> subscription;
+
+  //Data from firestore will be DocumentSnapshot
+  List<DocumentSnapshot> chatData;
+  int _lengthOfChatData = 0;
+  String userName = "";
+  String uid = "";
+
+  //Referencing the events collection
+  final CollectionReference collectionReference =
+      Firestore.instance.collection("chat");
+
   void initState() {
     super.initState();
+    subscription = collectionReference
+        .orderBy("timestamp", descending: true)
+        .limit(20)
+        .snapshots()
+        .listen((datasnapshot) {
+      setState(() {
+        var tChatData = datasnapshot.documents;
+        //Sort the data in descending order according to time of creation
+        tChatData
+            .sort((x, y) => y.data["timestamp"].compareTo(x.data["timestamp"]));
+        chatData = tChatData;
+        _lengthOfChatData = chatData.length;
+      });
+    });
+
+    setUserData();
+  }
+
+  setUserData() async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    setState(() {
+      userName = user.displayName;
+      uid = user.uid;
+    });
+  }
+
+  @override
+  void dispose() {
+    for (Msg msg in _messages) {
+      msg.animationController.dispose();
+    }
+    super.dispose();
   }
 
   @override
@@ -35,78 +81,123 @@ class _ChatPageState extends State<ChatPage> {
           },
         ),
       ),
-      body: Container(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.start,
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              itemCount: _lengthOfChatData,
+              reverse: true,
+              padding: EdgeInsets.all(6.0),
+              itemBuilder: (BuildContext context, int index) {
+                Msg msg = new Msg(
+                  text: "${chatData[index].data["text"]}",
+                  username: "${chatData[index].data["name"]}",
+                  animationController: new AnimationController(
+                      vsync: this, duration: Duration(milliseconds: 800)),
+                );
+                msg.animationController.forward();
+                return msg;
+              },
+            ),
+          ),
+          Divider(
+            height: 1.0,
+          ),
+          Container(
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                  child: Container(
+                    child: TextField(
+                      style: TextStyle(color: Colors.blue, fontSize: 15.0),
+                      controller: _textEditingController,
+                      decoration: InputDecoration.collapsed(
+                        hintText: 'Type your message...',
+                        hintStyle: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // Button send message
+                Material(
+                  child: new Container(
+                    margin: new EdgeInsets.symmetric(horizontal: 8.0),
+                    child: new IconButton(
+                      icon: new Icon(Icons.send),
+                      onPressed: handleMessageSubmit,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void handleMessageSubmit() async {
+    try {
+      await collectionReference.add({
+        'name': userName,
+        'text': _textEditingController.text,
+        'uid': uid,
+        'timestamp': DateTime.now().toString(),
+      }).catchError((err) {
+        print("Error $err");
+      });
+      _textEditingController.clear();
+    } catch (err) {
+      print("error ocured in ChatPage.dart in handle Message Submit");
+    }
+  }
+}
+
+class Msg extends StatelessWidget {
+  Msg({this.username, this.text, this.animationController});
+  final String text;
+  final String username;
+  final AnimationController animationController;
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+      sizeFactor: new CurvedAnimation(
+        parent: animationController,
+        curve: Curves.bounceOut,
+      ),
+      axisAlignment: 0.0,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Container(
-              height: 400.0,
-              child: StreamBuilder(
-                stream: Firestore.instance
-                    .collection('chat')
-                    .orderBy('timestamp', descending: true)
-                    .limit(20)
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
-                      ),
-                    );
-                  } else {
-                    return ListView.builder(
-                      padding: EdgeInsets.all(10.0),
-                      itemBuilder: (context, index) {
-                        return Container(
-                            alignment: Alignment.bottomLeft,
-                            child: Column(
-                              children: <Widget>[
-                                Text(
-                                    "${snapshot.data.documents[index].data["name"]}"),
-                                Text(
-                                    "${snapshot.data.documents[index].data["text"]}"),
-                              ],
-                            ));
-                      },
-                      itemCount: snapshot.data.documents.length,
-                      reverse: true,
-                    );
-                  }
-                },
-              ),
+              margin: EdgeInsets.only(right: 10.0),
+              child: Container(),
             ),
-            Container(
-              child: Row(
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Flexible(
-                    child: Container(
-                      child: TextField(
-                        style: TextStyle(color: Colors.blue, fontSize: 15.0),
-                        controller: textEditingController,
-                        decoration: InputDecoration.collapsed(
-                          hintText: 'Type your message...',
-                          hintStyle: TextStyle(color: Colors.blue),
-                        ),
-                      ),
-                    ),
+                  Text(
+                    "$username",
+                    style:
+                        TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),
                   ),
-
-                  // Button send message
-                  Material(
-                    child: new Container(
-                      margin: new EdgeInsets.symmetric(horizontal: 8.0),
-                      child: new IconButton(
-                        icon: new Icon(Icons.send),
-                        onPressed: () => {},
-                        color: Colors.blue,
-                      ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 6.0),
+                    child: Text(
+                      "$text",
+                      style: TextStyle(
+                          fontSize: 11.0, fontWeight: FontWeight.w400),
                     ),
-                    color: Colors.white,
-                  ),
+                  )
                 ],
               ),
-            ),
+            )
           ],
         ),
       ),

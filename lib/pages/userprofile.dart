85d076a30d17286b028f56c42f1432d3f4ca095d
Nityananda Gohain <nityanandagohain@gmail.com>
2019-01-17 23:53:15 +0530

    userprofile datadisplay bug fixed along with overflow bug while editing

--- lib/pages/userprofile.dart
+++ lib/pages/userprofile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../services/usermanagement.dart';
 
 class UserProfile extends StatefulWidget {
@@ -19,6 +20,7 @@ class _UserProfileState extends State<UserProfile> {
   String _name = "";
   String _rollno = "";
   String _hostel = "";
+  FirebaseUser user;
   bool edit = false;
   List<DocumentSnapshot> userData = [];
   void saveProfile() async {
@@ -33,16 +35,25 @@ class _UserProfileState extends State<UserProfile> {
     }
   }
 
+  void getUid() async {
+    user = await FirebaseAuth.instance.currentUser();
+  }
+
   @override
   void initState() {
     super.initState();
+    getUid();
     subscription = collectionReference.snapshots().listen((datasnapshot) {
-      setState(() {
-        userData = datasnapshot.documents;
-        _name = userData[0].data['name'];
-        _rollno = userData[0].data['rollno'];
-        _hostel = userData[0].data['hostel'];
-      });
+      userData = datasnapshot.documents;
+      for (var i = 0; i < userData.length; i++) {
+        if (userData[i].data['uid'] == user.uid) {
+          setState(() {
+            _name = userData[i].data['name'];
+            _rollno = userData[i].data['rollno'];
+            _hostel = userData[i].data['hostel'];
+          });
+        }
+      }
     });
   }
 
@@ -79,65 +90,69 @@ class _UserProfileState extends State<UserProfile> {
       ),
       body: Center(
         child: edit
-            ? Padding(
-                padding: const EdgeInsets.all(40.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      TextFormField(
-                        initialValue: _name,
-                        decoration: InputDecoration(
-                          labelText: "Name",
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
+            ? ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            initialValue: _name,
+                            decoration: InputDecoration(
+                              labelText: "Name",
+                              labelStyle: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.green),
+                              ),
+                            ),
+                            onSaved: (value) => _name = value,
                           ),
-                        ),
-                        onSaved: (value) => _name = value,
-                      ),
-                      TextFormField(
-                        initialValue: _rollno,
-                        decoration: InputDecoration(
-                          labelText: "Roll No",
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
+                          TextFormField(
+                            initialValue: _rollno,
+                            decoration: InputDecoration(
+                              labelText: "Roll No",
+                              labelStyle: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.green),
+                              ),
+                            ),
+                            onSaved: (value) => _rollno = value,
                           ),
-                        ),
-                        onSaved: (value) => _rollno = value,
-                      ),
-                      TextFormField(
-                        initialValue: _hostel,
-                        decoration: InputDecoration(
-                          labelText: "Hostel",
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
+                          TextFormField(
+                            initialValue: _hostel,
+                            decoration: InputDecoration(
+                              labelText: "Hostel",
+                              labelStyle: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.green),
+                              ),
+                            ),
+                            onSaved: (value) => _hostel = value,
                           ),
-                        ),
-                        onSaved: (value) => _hostel = value,
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          MaterialButton(
+                            child: Text("SUBMIT"),
+                            color: Colors.greenAccent,
+                            onPressed: saveProfile,
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      MaterialButton(
-                        child: Text("SUBMIT"),
-                        color: Colors.greenAccent,
-                        onPressed: saveProfile,
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                ],
               )
             : userData == []
                 ? Container()
